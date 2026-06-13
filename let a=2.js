6d4a5ceb63b7b9f4f5b7bdad032c8d045ba2a41f
hoolimallikarjun835-eng <hoolimallikarjun835@gmail.com>
2026-06-13 16:33:02 +0530

    Add function to classify JavaScript basics score

--- let a=2.js
+++ let a=2.js
@@ -1,1 +1,15 @@
-let a = 2;
+function classifyJsBasicsScore(score) {
+  if (score >= 90) {
+    return"EXCELLENT"
+  }
+   if (score >= 75) {
+    return "GOOD"
+  }
+  if (score >= 50) {
+    return "NEEDS_PRACTICE"
+  }
+  else {
+    return "REVISIT"
+  }
+}
+console.log(classifyJsBasicsScore(10))
